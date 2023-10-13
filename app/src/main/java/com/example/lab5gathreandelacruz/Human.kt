c9a61f95e61c48def4136c2eaef7c764050027cd
Gathrean Dela Cruz <gathrean@icloud.com>
2023-10-13 00:07:39 -0700

    Removed () in Minion()

--- app/src/main/java/com/example/lab5gathreandelacruz/Human.kt
+++ app/src/main/java/com/example/lab5gathreandelacruz/Human.kt
@@ -3,7 +3,7 @@ package com.example.lab5gathreandelacruz
 /**
  * Human class that inherits from Minion
  */
-class Human : Minion() {
+class Human : Minion {
     override val race: String = "Human"
     override val baseHealth: Int = 5
     override val baseSpeed: Int = 5
