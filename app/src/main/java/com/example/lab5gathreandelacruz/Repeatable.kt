8c40c975e0f4924d90ee4212f6bb3a9067121ed7
Gathrean Dela Cruz <gathrean@icloud.com>
2023-10-13 00:09:47 -0700

    Added abstract Int repeatNum in Repeatable interface

--- app/src/main/java/com/example/lab5gathreandelacruz/Repeatable.kt
+++ app/src/main/java/com/example/lab5gathreandelacruz/Repeatable.kt
@@ -1,5 +1,9 @@
 package com.example.lab5gathreandelacruz
 
+/**
+ * Interface for Repeatable tasks with a specified number
+ */
 interface Repeatable {
+    val repeatNum: Int
     fun repeat(times: Int, listener: MissionListener)
 }
